3bc799f07a7039255cfc941435aa92ebd89c4675
Marco ASDF <ciabo89@hotmail.com>
2017-04-04 22:05:33 +0200

    First Commit

---
+++ kalman_filter.cpp
@@ -0,0 +1,48 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+
+	//State transiction matrix F_ assumes a constant speed motion with dt dependancy
+	/*	1	0	dt	0
+		0	1	0	dt
+		0	0	1	0
+		0	0	0	1
+	*/
+	//Process covariance matrix assumes to have some acceleration noise Q_
+
+	x_ = F_ * x_;
+	P_ = F_ * P_ * F_.transpose() + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+	VectorXd y = z - H_ * x_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+}
---
+++ tools.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+using namespace std;
+
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth) {
+
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	// check the validity of the inputs 
+    if(estimations.size() == 0 || ground_truth.size() == 0 || estimations.size() != ground_truth.size())
+    {
+        cout << "Error with estimation and ground_truth size" << endl;
+        return rmse;
+    }
+	//accumulate squared residuals
+	for(int i=0; i < estimations.size(); ++i)
+		rmse = rmse.array() + (estimations[i]-ground_truth[i]).array()*(estimations[i]-ground_truth[i]).array();
+
+	//calculate the mean and the squared root
+    rmse /= estimations.size();
+    rmse = rmse.array().sqrt();
+
+	return rmse;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  	
+	MatrixXd Hj(3,4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	float pSum = pow(px,2) + pow(py,2);
+    float pSqrt = sqrt(pSum);
+    float ps32 = sqrt(pow(pSum,3));
+	
+	//check division by zero
+	
+    if(fabs(pSum) < 0.0001){
+		cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+		return Hj;
+	}
+	
+	//compute the Jacobian matrix
+    else
+        Hj << px/pSqrt,                 py/pSqrt,               0,          0,
+              -py/pSum,                 px/pSum,                0,          0,
+              (px*(vx*py-vy*px))/ps32,  (py*(vy*px-vx*py))/ps32,px/pSqrt,   py/pSqrt;
+    
+	return Hj;
+
+}
